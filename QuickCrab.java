1515e73b772c766036914b0ece88a8e483cc02c1
A-Savchenko <122913879+A-Savchenko@users.noreply.github.com>
2023-03-30 09:57:00 -0700

    Add files via upload

--- QuickCrab.java
+++ QuickCrab.java
@@ -13,11 +13,29 @@ public class QuickCrab extends CrabCritter
         ArrayList<Location> locs = new ArrayList<Location>();
         int[] dirs =
                 { Location.LEFT, Location.RIGHT };
-
-//        for (Location loc : getLocationsInDirections(dirs))
-//            if (getGrid().get(loc) == null)
-//                locs.add(loc);
-
+        Location current = getLocation(); 
+        for (Location loc : getLocationsInDirections(dirs))
+        {
+            if(loc.equals(current)) continue; 
+            if (getGrid().get(loc) == null)
+                locs.add(loc);
+		}
         return locs;
-    }
+	}
+	public ArrayList<Location> getLocationsInDirections(int[] directions)
+    {
+        ArrayList<Location> locs = new ArrayList<Location>();
+        Grid gr = getGrid();
+        Location loc = getLocation();
+		int[] dirs =
+                { Location.LEFT, Location.RIGHT };
+        for (int d : dirs)
+        {	
+			Location neighborLoc = loc.getAdjacentLocation(getDirection()+d).getAdjacentLocation(getDirection()+d);
+           
+            if (gr.isValid(neighborLoc))
+                locs.add(neighborLoc);
+        }
+        return locs;
+    }    
 }
